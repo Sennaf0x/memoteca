d731e17aca6747098f2638bdf34bb32f0b93c6d6
Sennaf0x <Sennafo@hotmail.com>
2023-09-26 22:56:48 -0400

    Criando backend e usando observables para criar métodos de get e post

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -7,27 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: "Passo informações para o componente filho",
-      autoria: "Pai",
-      modelo:"modelo1"
-    },
-    {
-      conteudo: "Preciso do @Input para passar informações",
-      autoria: "Filho",
-      modelo:"modelo3"
-    },
-    {
-      conteudo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
-      autoria: "Álguem",
-      modelo:"modelo2"
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    } )
   }
 
 }
