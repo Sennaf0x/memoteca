d6896a412b0a7343f09c46e17fc60aa5044ca8f2
Sennaf0x <Sennafo@hotmail.com>
2023-09-26 21:45:16 -0400

    Aprendendo a utilizar as diretivas ngIF, ngFor e ngClass

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentosComponent implements OnInit {
       conteudo: "Preciso do @Input para passar informações",
       autoria: "Filho",
       modelo:"modelo3"
+    },
+    {
+      conteudo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+      autoria: "Álguem",
+      modelo:"modelo2"
     }
   ];
 
